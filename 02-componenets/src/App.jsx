1a8d7b403df0deab64d7bad7ad6f48d2843eef6b
HarshVerma2705 <hv152775@gmail.com>
2025-10-27 09:52:55 +0530

    abhi kuch naye folder add kiye hai

---
+++ 02-componenets/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './components/Cards'
+import Navbar from './components/nav'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ 02-componenets/src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div>
+      <div className="card">
+      <h2>Sarthak Sharma</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt fugiat.</p>
+    </div>
+    </div>
+  )
+}
+
+export default Cards
